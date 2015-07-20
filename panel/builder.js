34551a9ed3361bd3d6cf3023d4c135f53429e345
Johnny Wu <KarasAya@gmail.com>
2015-07-20 19:28:09 +0800

    support scene list and start scene

--- panel/builder.js
+++ panel/builder.js
@@ -8,29 +8,69 @@ Editor.registerPanel( 'builder.panel', {
     is: 'editor-builder',
 
     properties: {
-        platform: {
-            type: String,
-            value: 'web-mobile'
+        platformList: {
+            type: Array,
+            value: function () {
+                return [
+                    { value: 'web-mobile', text: 'Web Mobile' },
+                    { value: 'web-desktop', text: 'Web Desktop' },
+                ];
+            },
         },
-        buildPath: {
-            type: String,
-            value: '',
-        },
-        projectInfo: {
-            type: Object,
+
+        sceneList: {
+            type: Array,
             value: function () {
                 return [];
-            }
+            },
         },
-        isDebug: {
-            type: Boolean,
-            value: true,
-        }
     },
 
+    observers: [
+        '_projectProfileChanged(profiles.project.*)'
+    ],
+
     ready: function () {
-        this.projectInfo = Editor.projectInfo;
-        this.buildPath = this.projectInfo.path;
+        this.async( function () {
+            var self = this;
+
+            if ( !self.profiles.project.title ) {
+                self.set('profiles.project.title', Editor.projectInfo.name );
+            }
+            if ( !self.profiles.project.buildPath ) {
+                self.set('profiles.project.buildPath',
+                         Path.join( Editor.projectInfo.path, 'build', self.profiles.project.title ) );
+            }
+            Editor.assetdb.queryAssets( 'assets://**/*', 'scene', function ( results ) {
+                // clean up excludeScenes
+                var needSave = false;
+                self.profiles.project.excludeScenes.forEach(function ( excludeScene, index ) {
+                    var found = results.some( function ( result ) {
+                        return result.uuid === excludeScene;
+                    });
+
+                    if ( !found ) {
+                        self.profiles.project.excludeScenes.splice( index, 1 );
+                        needSave = true;
+                    }
+                });
+                if ( needSave ) {
+                    self.profiles.project.save();
+                }
+
+                self.sceneList = results.map( function ( result ) {
+                    return {
+                        value: result.uuid,
+                        text: result.name,
+                        checked: self.profiles.project.excludeScenes.indexOf(result.uuid) === -1
+                    };
+                });
+
+                if ( !self.profiles.project.startSceneUuid && self.sceneList.length > 0 ) {
+                    self.set('profiles.project.startSceneUuid', self.sceneList[0].value );
+                }
+            });
+        });
     },
 
     _onChooseDistPathClick: function (event) {
@@ -39,14 +79,12 @@ Editor.registerPanel( 'builder.panel', {
         var dialog = Remote.require('dialog');
         var projectPath = Editor.projectInfo.path;
 
-        dialog.showOpenDialog({ defaultPath: projectPath, properties: ['openDirectory']},function (res) {
+        dialog.showOpenDialog({
+            defaultPath: projectPath,
+            properties: ['openDirectory']
+        }, function (res) {
             if (res) {
-                if (this.platform === 'web-mobile') {
-                    this.buildPath = res + '/mobile-' + Path.basename(projectPath);
-                }
-                else {
-                    this.buildPath = res + '/desktop-' + Path.basename(projectPath);
-                }
+                this.buildPath = Path.join( res[0], 'build', this.profiles.project.title );
             }
         }.bind(this));
     },
@@ -70,25 +108,53 @@ Editor.registerPanel( 'builder.panel', {
         Shell.beep();
     },
 
-    _onCloseWindowClick: function (event) {
-        event.stopPropagation();
-
-        window.close();
-    },
-
     _onBuildClick: function (event) {
         event.stopPropagation();
-        var buildUuidList = Editor.Selection.curSelection('asset');
-        if (buildUuidList.length > 0) {
-            Editor.sendToCore('app:build-project', this.platform, this.buildPath, buildUuidList, {
-                projectName: this.projectInfo.name,
-                isDebug: this.isDebug
-            });
 
-            window.close();
+        var buildUuidList = this.sceneList.filter( function ( scene ) {
+            return scene.checked;
+        }).map( function ( scene ) {
+            return scene.value;
+        });
+
+        if (buildUuidList.length > 0) {
+            Editor.sendToCore('app:build-project', {
+                title: this.profiles.project.title,
+                platform: this.profiles.project.platform,
+                buildPath: this.profiles.project.buildPath,
+                startScene: this.profiles.project.startScene,
+                scenes: buildUuidList,
+                debug: this.profiles.project.debug,
+            });
         }
         else {
             Editor.error('Please select scenes to build');
+        }
+    },
+
+    _isStartScene: function ( startScene, value ) {
+        return startScene === value;
+    },
+
+    _onCheckedChanged: function () {
+        var model = this.$.sceneList.modelForElement(event.target);
+        var idx = this.profiles.project.excludeScenes.indexOf( model.item.value );
+
+        if ( model.item.checked ) {
+            if ( idx !== -1 ) {
+                this.splice( 'profiles.project.excludeScenes', idx, 1 );
+            }
+        }
+        else {
+            if ( idx === -1 ) {
+                this.push( 'profiles.project.excludeScenes', model.item.value );
+            }
+        }
+    },
+
+    _projectProfileChanged: function ( changeRecord ) {
+        if ( this.profiles.project.save ) {
+            this.profiles.project.save();
         }
     },
 });
